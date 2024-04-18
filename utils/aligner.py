0ddd91d19a46d40c980638fe7e9509c290f3d93e
Jiwoo Lee <jiwookki@gmail.com>
2024-04-19 01:18:48 +0800

    new style health and health system

--- utils/aligner.py
+++ utils/aligner.py
@@ -3,13 +3,13 @@ import pygame
 OUTPUT_FILENAME = "healthbar.PNG"
 INPUT_FILENAME = "heart.PNG"
 
-REPEATS = 5
-SPACING = 16
+REPEATS = 6
+SPACING = -4
 
 input_file = pygame.image.load(INPUT_FILENAME)
 
 output_surface = pygame.Surface(((input_file.get_width() + SPACING) * REPEATS + SPACING, input_file.get_height()), flags=pygame.SRCALPHA)
-#output_surface.fill
+output_surface.fill([230, 20, 20])
 
 counter = SPACING
 for x in range(0, REPEATS):
