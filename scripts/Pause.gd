17860b2eafc2e4b20bbaf6c41a02f151a360b793
jiwookki <jiwookki@gmail.com>
2025-07-06 04:04:53 +0800

    add pause function

---
+++ scripts/Pause.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if Input.is_action_just_pressed("pause"):
+		visible = !visible
+		get_tree().paused = !get_tree().paused
+	if Input.is_action_just_pressed("pause_quit") and get_tree().paused:
+		get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
